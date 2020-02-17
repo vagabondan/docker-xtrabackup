ab4ca1f2cede5c19d24fad074899b27a60e4abfc
Vagabondan <Strannix.Vagabondan@gmail.com>
2020-02-17 15:41:00 +0300

    Minor fix

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it \
--v /Users/Strannix/Structure/projects/docker/percona/zbx_env/var/lib/mysql:/var/lib/mysql \
--v /Users/Strannix/Structure/projects/docker/docker-xtrabackup/backup:/backups \
---rm=true \
---network=zbx_net_frontend \
-vagabondan/xtrabackup \
-sh -c "exec /xtrabackup.sh $1"
