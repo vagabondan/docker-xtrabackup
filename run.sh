c9fc0d0a00d0043a53af8422d3274aabb44edc04
Vagabondan <Strannix.Vagabondan@gmail.com>
2020-02-20 11:34:03 +0300

    Minor fix for beauty

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 docker run -it \
+-v `pwd`/backups:/backups \
 -v `pwd`/../percona/zbx_env/var/lib/mysql:/var/lib/mysql \
 -v `pwd`/../percona1/zbx_env/var/lib/mysql:/restore/mysql \
--v `pwd`/backups:/backups \
 --rm=true \
---network=zbx_net_frontend \
 --entrypoint="" \
 vagabondan/xtrabackup \
 sh -c "exec /xtrabackup.sh $*"
